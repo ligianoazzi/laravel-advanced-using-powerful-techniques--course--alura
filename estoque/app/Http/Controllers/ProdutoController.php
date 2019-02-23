e14ced138d8b7143593ee80aad1181c67db2f171
Ligiano Azzi <ldaazzi@gmail.com>
2019-02-23 11:52:59 +0000

    creating app/Produtos model and improving ProdutoController

--- estoque/app/Http/Controllers/ProdutoController.php
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -4,11 +4,17 @@ namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Request;
+use estoque\Produto; // calling for the model
+
+
+
+
 class ProdutoController extends Controller
 {
     public function lista(){
 
-   	 $produtos = DB::select('select * from produtos');
+   	 //$produtos = DB::select('select * from produtos');
+    	$produtos = Produto::all();
    	 return view('listagem')->with('produtos', $produtos);
 
     }
@@ -16,11 +22,15 @@ class ProdutoController extends Controller
     public function mostra(){
     	$id = Request::route('id');
 
-		$produto = DB::select('select * from produtos where id = ?', [$id]);
+		//$produto = DB::select('select * from produtos where id = ?', [$id]);
+		$produto = Produto::find($id);
+
+
     	if(empty($produto)){
     		return "This product does not exist";
     	}
-	   	return view('detalhes')->with('p', $produto[0]);
+	   	//return view('detalhes')->with('p', $produto[0]);
+	   	return view('detalhes')->with('p', $produto); // is not more a array
 
     }
 
@@ -30,14 +40,26 @@ class ProdutoController extends Controller
 
 	public function adiciona(){
 
-		$nome = Request::input('nome');
+		/*$nome = Request::input('nome');
 		$quantidade = Request::input('quantidade');
 		$valor = Request::input('valor');
-		$descricao = Request::input('descricao');
+		$descricao = Request::input('descricao');*/
+
+		// $table = 'nome_of_the_table';
+
+		$produto = new Produto();
+		$produto->nome = Request::input('nome');
+		$produto->quantidade = Request::input('quantidade');
+		$produto->valor = Request::input('valor');
+		$produto->descricao = Request::input('descricao');
+
+		$produto->timestamps = false; // in the others versions of Laravel, can be different
+
 
 		try {
 
-			DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?, ?, ?, ?)', array($nome, $quantidade, $valor, $descricao));
+			//DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?, ?, ?, ?)', array($nome, $quantidade, $valor, $descricao));
+			$produto->save();			
 
 			//return redirect('/produtos')->withInput(Request::only('nome'));
 			return redirect()
